3e16e47cded519a932840faf65b51e8e60a500c6
ywhan <ywhan@ibizsoftware.net>
2018-11-07 10:24:15 +0900

    Start Project

---
+++ applicationServer/src/main/java/com/ApplicationServer.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationServer.class, args);
+	}
+}
